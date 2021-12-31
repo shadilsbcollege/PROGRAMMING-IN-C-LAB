399902e61549a0003f2932f1169e1239f2c80a11
shadilsbcollege <96900919+shadilsbcollege@users.noreply.github.com>
2021-12-31 19:07:51 +0530

    Create PATTERN3.C

---
+++ PATTERN3.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+	int i,j,n;
+	clrscr();
+	printf("Enter a number\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		{
+			for(j=i;j<=n;j++)
+			{
+				printf("%d",j);
+			}
+				printf("\n");
+		}
+	getch();
+}
