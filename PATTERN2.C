2aa1c7ae81ea6bd6929b19d0974699bc41234ffe
shadilsbcollege <96900919+shadilsbcollege@users.noreply.github.com>
2021-12-31 19:04:55 +0530

    Add files via upload

---
+++ PATTERN2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,n;
+	printf("enter the number\n");
+	scanf("%d",&n);p
+	for(i=1;i<=n;i++)
+	{
+	for(j=1;j<=n;j++)
+	{
+	printf("%d",i);
+	}
+	printf("\n");
+	}
+	getch();
+	}
