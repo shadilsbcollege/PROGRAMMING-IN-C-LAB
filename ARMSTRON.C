fce68dcb1395dbd18757136d678565c3ffeed03c
shadilsbcollege <96900919+shadilsbcollege@users.noreply.github.com>
2022-01-04 07:49:54 +0530

    Add files via upload

---
+++ ARMSTRON.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0,r,temp,c;
+clrscr();
+printf("enter a number;");
+scanf("%d",&n);
+temp=n;
+while(n>0)
+{
+	r=n%10;
+	c=r*r*r;
+	sum=sum+c;
+	n=n/10;
+	}
+	if(sum==temp)
+	{
+	printf("%d is an armstrong number:",temp);
+	}
+	else
+	{
+	printf("%d is not an armstrong number:",temp);
+	}
+	getch();
+	}
